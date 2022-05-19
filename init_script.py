9cd2982d221283862e79c5ff3e5eed973986009c
AlexBrence <alexbrence@protonmail.com>
2022-05-19 14:41:41 +0200

    bug fixes, easier to use

--- init_script.py
+++ init_script.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 try:
-    sys.path.append(glob.glob('../PythonAPI/carla/dist/carla-*%d.%d-%s.egg' % (
+    sys.path.append(glob.glob('./PythonAPI/carla/dist/carla-*%d.%d-%s.egg' % (
         sys.version_info.major,
         sys.version_info.minor,
         'win-amd64' if os.name == 'nt' else 'linux-x86_64'))[0])
@@ -16,26 +16,9 @@ import argparse
 
 def main():
     argparser = argparse.ArgumentParser(description='Init settings')
-    argparser.add_argument(
-        '-r',
-        '--render',
-        default=True,
-        dest='render',
-        type=bool,
-        help='True/False, default = True')
-    argparser.add_argument(
-        '-l',
-        '--list-towns',
-        action='store_true',
-        dest='list',
-        help='List towns')
-    argparser.add_argument(
-        '-t',
-        '--town',
-        default='/Game/Carla/Maps/Town03',
-        dest='town',
-        type=str,
-        help='Choose town (use -l to list the options)')
+    argparser.add_argument('-r', '--render', action='store_true', dest='render', help='If this flag is used then it will render')
+    argparser.add_argument('-l', '--list-towns', action='store_true', dest='list', help='List towns')
+    argparser.add_argument('-t', '--town', dest='town', type=str, help='Choose town (use -l to list the options)')
     args = argparser.parse_args()
 
     client = carla.Client('localhost', 2000)
@@ -44,20 +27,25 @@ def main():
     settings = world.get_settings()
 
     if not args.render:
+        print('### Rendering disabled')
         settings.no_rendering_mode = True
     else:
+        print('### Rendering enabled')
         settings.no_rendering_mode = False
     world.apply_settings(settings)
 
     if args.list:
-        print(f'Available maps: {client.get_available_maps()}')
+        print(f'### Available maps:\n{client.get_available_maps()}')
     if args.town:
         if not args.town in client.get_available_maps():
-            print('This town does not exist. Run \'python3 init_script.py -l\' to see available towns.')
+            print('### This town does not exist. Run \'python3 init_script.py -l\' to see available towns.')
         else:
+            print(f'### Loading new world...')
             world = client.load_world(args.town)
             world.unload_map_layer(carla.MapLayer.Buildings)
             world.unload_map_layer(carla.MapLayer.ParkedVehicles)
+            print('### Done')
+
 
 
 if __name__ == '__main__':
