0cb4d2d654f4a17d1768c810c09162161b17b5b7
santaclz <rabit0110115@gmail.com>
2022-06-01 23:56:54 +0200

    added lidar

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ import carla
 import random
 import queue
 import numpy as np
+import copy
 import cv2
 import time
 import torch
@@ -25,6 +26,8 @@ import lane_detection
 import detect_trafficLights
 import drivable_detect
 
+WIDTH = 640
+HEIGHT = 384
 # Load model
 model = torch.hub.load('ultralytics/yolov5', 'yolov5s')  # yolov5n, yolov5x6, custom
 torch.save(model, 'model.pth')
@@ -74,22 +77,54 @@ if __name__ == "__main__":
 
     vehicle.set_autopilot(True)
 
-    # Get camera image
+    ### CAMERA
     camera_bp = blueprint_library.find('sensor.camera.rgb')
     # Modify the attributes of the blueprint to set image resolution and field of view.
-    camera_bp.set_attribute('image_size_x', '640')
-    camera_bp.set_attribute('image_size_y', '384')
-    
+    camera_bp.set_attribute('image_size_x', f'{WIDTH}')
+    camera_bp.set_attribute('image_size_y', f'{HEIGHT}')
+
     camera_transform = carla.Transform(carla.Location(x=1.5, z=2.4))
     camera = world.spawn_actor(camera_bp, camera_transform, attach_to=vehicle)
     image_queue = queue.LifoQueue()
     camera.listen(image_queue.put)
     actor_list.append(camera)
 
+    ### LIDAR
+    lidar_bp = blueprint_library.find('sensor.lidar.ray_cast')
+    # Modify the attributes of the blueprint to set image resolution and field of view.
+    lidar_bp.set_attribute('rotation_frequency', '10')
+
+    lidar_transform = carla.Transform(carla.Location(x=1.5, z=2.4))
+    lidar = world.spawn_actor(lidar_bp, lidar_transform, attach_to=vehicle)
+    lidar_queue = queue.LifoQueue()
+    lidar.listen(lidar_queue.put)
+    actor_list.append(lidar)
+    
+
     # Main loop
     while True:
         vehicles = []
 
+        ### LIDAR logic
+        lidar_measurement = lidar_queue.get()
+        for location in lidar_measurement:
+            print(location)
+
+        lidar_range = 50
+        points = np.frombuffer(lidar_measurement.raw_data, dtype=np.dtype('f4'))
+        points = np.reshape(points, (int(points.shape[0] / 4), 4))
+        lidar_data = np.array(points[:, :2])
+        lidar_data *= min(WIDTH, HEIGHT) / (2.0 * lidar_range)
+        lidar_data += (0.5 * WIDTH, 0.5 * HEIGHT)
+        lidar_data = np.fabs(lidar_data)  # pylint: disable=E1111
+        lidar_data = lidar_data.astype(np.int32)
+        lidar_data = np.reshape(lidar_data, (-1, 2))
+        lidar_img_size = (WIDTH, HEIGHT, 3)
+        lidar_img = np.zeros((lidar_img_size), dtype=np.uint8)
+        lidar_img[tuple(lidar_data.T)] = (255, 255, 255)
+        cv2.imshow("lidar_frame", lidar_img)
+
+        ### IMG logic
         image = image_queue.get()
         data = to_rgb_array(image)
 
@@ -110,10 +145,14 @@ if __name__ == "__main__":
         #data_together = cv2.bitwise_or(data_drivable, tmp_data)
         objects = object_detection.get_object(model, data)
         try:
-            # data_lane = lane_detection.draw_lane(data)
+            ### Detect lines
+            data_lane = copy.deepcopy(data_drivable)
+            data_lane[:,:,1] = 0
+            #data_lane_lines = lane_detection.draw_lane(data_lane)
+            #cv2.imshow("frame2", data_lane_lines)
+            cv2.imshow("frame2", data_lane)
             #cv2.imshow("frame", data_together)
-            #data_lane = lane_detection.draw_lane(data)
-            #cv2.imshow("frame", data_lane)
+
 
             drawn_trafficlights = detect_trafficLights.get_trafficlights_drawn(rgb_img, bgr_img, recognized_objects)
             data_together = cv2.bitwise_or(drawn_trafficlights, data_drivable)
@@ -126,6 +165,7 @@ if __name__ == "__main__":
             print("exception caught!")
 
         cv2.imshow("frame", data_together)
+        #cv2.imshow("frame2", data_lane)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
