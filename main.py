1ac9bb5e8f09a8f47cbf54c3c5d569b733c0b05a
AlexBrence <alexbrence@protonmail.com>
2022-06-06 14:49:25 +0200

    removed init with .egg file part

--- main.py
+++ main.py
@@ -2,14 +2,6 @@ import os
 import glob
 import sys
 
-try:
-    sys.path.append(glob.glob('../PythonAPI/carla/dist/carla-*%d.%d-%s.egg' % (
-        sys.version_info.major,
-        sys.version_info.minor,
-        'win-amd64' if os.name == 'nt' else 'linux-x86_64'))[0])
-except IndexError:
-    pass
-
 
 import carla
 import random
